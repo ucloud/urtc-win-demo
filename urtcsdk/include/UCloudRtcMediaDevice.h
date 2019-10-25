da52577dd931b08d63fa7b92d36a985cbfd9c551
martin <martin.wang@ucloud.cn>
2019-10-25 17:52:33 +0800

    替换头文件

--- urtcsdk/include/UCloudRtcMediaDevice.h
+++ urtcsdk/include/UCloudRtcMediaDevice.h
@@ -40,16 +40,19 @@ public:
 	virtual int getRecordingDeviceVolume(int *volume) = 0;
 	virtual int setRecordingDeviceVolume(int volume) = 0;
 
-	virtual int startCamTest(const char deviceId[MAX_DEVICE_ID_LENGTH], eUCloudRtcVideoProfile profile, void* hwnd) = 0;
+	virtual int startCamTest(const char deviceId[MAX_DEVICE_ID_LENGTH], 
+		eUCloudRtcVideoProfile profile,
+		void* hwnd) = 0;
 	virtual int stopCamTest() = 0;
 
-	virtual int startRecordingDeviceTest(const char deviceId[MAX_DEVICE_ID_LENGTH], UCloudRtcAudioLevelListener* audiolevel) = 0;
+	virtual int startRecordingDeviceTest(UCloudRtcAudioLevelListener* audiolevel) = 0;
 	virtual int stopRecordingDeviceTest() = 0;
 
-	virtual int startPlaybackDeviceTest(const char deviceId[MAX_DEVICE_ID_LENGTH], const char* testAudioFilePath) = 0;
+	virtual int startPlaybackDeviceTest(const char* testAudioFilePath) = 0;
 	virtual int stopPlaybackDeviceTest() = 0;
 
-	virtual int startCaptureFrame(eUCloudRtcVideoProfile profile,UCloudRtcVideoFrameObserver* observer) = 0;
+	virtual int startCaptureFrame(eUCloudRtcVideoProfile profile,
+		UCloudRtcVideoFrameObserver* observer) = 0;
 	virtual int stopCaptureFrame() = 0;
 
 protected:
